7ae7025fc012846109955c67ec298d6b711c26a5
William Forslund <b19wilfo@student.his.se>
2020-04-28 09:32:20 +0200

    Added network code

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,7 +2,20 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +24,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    MyAsyncTask extends AsyncTask
+
+    {public void doInBackground(){
+        @SuppressLint("StaticFieldLeak")
+        private class JsonTask extends AsyncTask<String, String, String> {
+
+            private HttpURLConnection connection = null;
+            private BufferedReader reader = null;
+
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Downloading JSON");
+                progressDialog.setCancelable(true);
+                progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        cancel(true);
+                    }
+                });
+                progressDialog.show();
+            }
+
+            protected String doInBackground(String... params) {
+                try {
+                    URL url = new URL("https://wwwlab.iit.his.se/brom/kurser/mobilprog/jsonservice.php");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+
+                    InputStream stream = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuilder builder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null && !isCancelled()) {
+                        builder.append(line).append("\n");
+                    }
+                    return builder.toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(s);
+                Log.d("brom","DataFetched:"+s);
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+                textView.setText(result);
+            }
+        }
+        // }
+        //public void onPostExecute(String s)
+    }
+        // {
+        // your code, parse the returned JSON in s}}
 }
